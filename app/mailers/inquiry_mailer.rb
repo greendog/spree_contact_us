e5808920608a6bbd1c20c8515b99cbe0d6084900
greendog <alexander.negoda@gmail.com>
2011-10-25 22:11:15 +0400

    fix ActionMailer bug

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,12 +1,15 @@
 class InquiryMailer < ActionMailer::Base
 
-  def notification(inquiry, resend=false)
-    subject "#{Spree::Config[:site_name]} : #{I18n.t(:contact_form)}"
-    body :inquiry => inquiry
-    recipients MailMethod.current.preferred_intercept_email
-    bcc "#{MailMethod.current.preferred_mail_bcc}, #{inquiry.email}"
-    from inquiry.email
-    content_type "text/html"
+  def notification(inquiry)
+    recipients = MailMethod.current.preferred_mail_bcc.split()
+    recipients = MailMethod.current.preferred_intercept_email unless MailMethod.current.preferred_intercept_email.blank?
+    @inquiry = inquiry
+    mail(
+        :from => inquiry.email,
+        :to => inquiry.email,
+        :subject => "#{Spree::Config[:site_name]} : #{I18n.t(:contact_form)}",
+        :bcc => recipients
+    )
   end
 
 end

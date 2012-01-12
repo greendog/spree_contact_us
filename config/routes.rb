cf8e1c5f2f8f89a7018e88a88f55ee83fe98cf25
Sam Beam <sbeam@onsetcorps.net>
2012-01-11 20:13:01 -0500

    use new engine-y routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-Rails.application.routes.draw do
-  match '/contact' => 'inquiries#new', :as => :contact
-
+Spree::Core::Engine.routes.append do
   resources :inquiries
 
   namespace :admin do
     resources :inquiries
     resource :inquiry_settings
   end
+
+  match '/contact' => 'inquiries#new', :as => :contact
 
 end

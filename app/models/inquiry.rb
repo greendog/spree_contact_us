0be738e38537e5f44aa0d5daa9fc687b28f7399c
Sam Beam <sbeam@onsetcorps.net>
2012-01-17 22:05:04 -0500

    whitespace

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,7 +3,7 @@ class Inquiry < ActiveRecord::Base
   validates :name, :message, :presence => true
   validates :email,        :format => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
    
- after_save :deliver_notification_email
+  after_save :deliver_notification_email
 
 
   def deliver_notification_email

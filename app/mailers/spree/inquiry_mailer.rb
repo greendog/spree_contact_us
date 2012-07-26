0dbdf5478791ac0e9f0fcdea2dc1473960aad543
Michael Bianco <iloveitaly@gmail.com>
2012-07-26 12:24:32 -0400

    Changed sending login. Updated subject.
    * Two emails are sent, not one. Although this doubles the load on the SMTP server
      it also makes it easier to handle support requests: the from field on the email
      sent to the preferred_mails_from is properly set to the request's email
    * Subject includes the support type

--- app/mailers/spree/inquiry_mailer.rb
+++ app/mailers/spree/inquiry_mailer.rb
@@ -2,14 +2,16 @@ module Spree
   class InquiryMailer < ActionMailer::Base
     default :from => Spree::MailMethod.current.preferred_mails_from
 
-    def notification(inquiry)
-      recipients = Spree::MailMethod.current.preferred_mail_bcc.split()
-      recipients = Spree::MailMethod.current.preferred_intercept_email unless Spree::MailMethod.current.preferred_intercept_email.blank?
+    def notification(inquiry, customer = false)
       @inquiry = inquiry
+
+      recipients = Spree::MailMethod.current.preferred_intercept_email || Spree::MailMethod.current.preferred_mail_bcc.split()
+
       mail(
-        :to => inquiry.email,
-        :subject => "#{Spree::Config[:site_name]}: #{I18n.t(:contact_form)}",
-        :bcc => recipients
+        :to => customer ? inquiry.email : recipients,
+        :subject => "#{Spree::Config[:site_name]} #{I18n.t(:contact_form)}: #{@inquiry.inquiry_type}",
+        :reply_to => customer ? Spree::MailMethod.current.preferred_mails_from : inquiry.email,
+        :bcc => ''
       )
     end
   end

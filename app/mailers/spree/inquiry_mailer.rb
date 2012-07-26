32a96daa69f86745f4822de4752855a260c722ae
Michael Bianco <iloveitaly@gmail.com>
2012-07-26 14:47:57 -0400

    Switched from reply-to to from
    reply-to is not respected in gmail if the from is the email being delivered to.
    gc Switched from reply-to to from

--- app/mailers/spree/inquiry_mailer.rb
+++ app/mailers/spree/inquiry_mailer.rb
@@ -5,13 +5,15 @@ module Spree
     def notification(inquiry, customer = false)
       @inquiry = inquiry
 
-      recipients = Spree::MailMethod.current.preferred_intercept_email || Spree::MailMethod.current.preferred_mail_bcc.split()
+      recipients = Spree::MailMethod.current.preferred_intercept_email || Spree::MailMethod.current.preferred_mail_bcc.split
+
+      # rather use reply-to for spam reasons (the email really isn't coming from the user)
+      # but it's not possible: http://productforums.google.com/forum/#!topic/gmail/azLFdnPfRZo
 
       mail(
         :to => customer ? inquiry.email : recipients,
         :subject => "#{Spree::Config[:site_name]} #{I18n.t(:contact_form)}: #{@inquiry.inquiry_type}",
-        :reply_to => customer ? Spree::MailMethod.current.preferred_mails_from : inquiry.email,
-        :bcc => ''
+        :from => customer ? Spree::MailMethod.current.preferred_mails_from : inquiry.email
       )
     end
   end

359bfbc45be76c54222d6c6fba54e0af5abe06a2
Sam Beam <sbeam@onsetcorps.net>
2012-10-26 22:44:52 -0400

    normalize mailer BCC logic now that #1806 is fixed

--- app/mailers/spree/inquiry_mailer.rb
+++ app/mailers/spree/inquiry_mailer.rb
@@ -4,18 +4,14 @@ module Spree
     def notification(inquiry, customer = false)
       @inquiry = inquiry
 
-      recipients = Spree::MailMethod.current.preferred_intercept_email || Spree::MailMethod.current.preferred_mail_bcc.split
-
       # rather use reply-to for spam reasons (the email really isn't coming from the user)
       # but it's not possible: http://productforums.google.com/forum/#!topic/gmail/azLFdnPfRZo
 
       mail(
-        :to => customer ? inquiry.email : recipients,
-        :subject => "#{Spree::Config[:site_name]} #{I18n.t(:contact_form)}: #{@inquiry.inquiry_type}",
-        :from => customer ? Spree::MailMethod.current.preferred_mails_from : inquiry.email,
-
-        # TODO UPGRADE_CHECK once https://github.com/spree/spree/pull/1806 this will work
-        :bcc => ''
+        :to => inquiry.email,
+        :subject => "#{Spree::Config[:site_name]} #{I18n.t(:contact_form)}: #{inquiry.inquiry_type}",
+        :from => Spree::MailMethod.current.preferred_mails_from,
+        :bcc => Spree::MailMethod.current.preferred_mail_bcc
       )
     end
 
